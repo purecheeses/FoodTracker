43fd28cb448d697049e32b8f0c22b52c08ba01ee
xin.luo <xin.luo@happyelements.com>
2018-06-13 18:49:58 +0800

    init

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by happyelements on 2018/6/8.
+//  Copyright © 2018年 happyelements. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
